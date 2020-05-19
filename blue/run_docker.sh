de62e028df472adf3ce2aded553f92e887382ebb
samhmariam <samhmariam@gmail.com>
2020-05-19 17:03:42 +0100

    update to docker build

--- blue/run_docker.sh
+++ blue/run_docker.sh
@@ -8,8 +8,8 @@ docker build --tag=testblueimage .
 
 # Step 2: 
 # List docker images
-docker image ls
+# docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 testblueimage
+# docker run -p 8000:80 testblueimage
--- green/run_docker.sh
+++ green/run_docker.sh
@@ -8,8 +8,8 @@ docker build --tag=testgreenimage .
 
 # Step 2: 
 # List docker images
-docker image ls
+# docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 testgreenimage
+# docker run -p 8000:80 testgreenimage
